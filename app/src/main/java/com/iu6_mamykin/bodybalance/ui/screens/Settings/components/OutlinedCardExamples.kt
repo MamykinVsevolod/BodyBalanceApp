0acd59867952ffe939b54c413271805005620cb8
MamykinVsevolod <vsevolodmamykin@gmail.com>
2024-11-09 18:08:31 +0300

    separate into components

---
+++ app/src/main/java/com/iu6_mamykin/bodybalance/ui/screens/Settings/components/OutlinedCardExamples.kt
@@ -0,0 +1,59 @@
+package com.iu6_mamykin.bodybalance.ui.screens.Settings.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.iu6_mamykin.bodybalance.R
+
+@Composable
+fun OutlinedCardExamples() {
+    OutlinedCard(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+        ),
+        shape = RoundedCornerShape(12),
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.outline),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 23.dp, start = 12.dp, end = 12.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Названия упражнений",
+                modifier = Modifier
+                    .padding(top = 28.dp, start = 16.dp, bottom = 28.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium
+            )
+            IconButton(
+                onClick = { /* do something */ }) {
+                Icon(
+                    painterResource(R.drawable.arrow_forward_icon),
+                    contentDescription = "Вперед"
+                )
+            }
+        }
+    }
+}
